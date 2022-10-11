78a7201a79fb5085a658166236a25e56e84a2853
Yury <ydaszukiewicz@gmail.com>
2022-10-11 21:21:31 +0300

    11.10 codeWars

--- start.js
+++ start.js
@@ -49,3 +49,67 @@
   
 //     return result.join("-")
 //   }
+
+
+
+// //the feast of many Beats
+// function feast(beast, dish) {
+//   if (beast[0] == dish[0] && beast[beast.length -1] == dish[dish.length -1]){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+// sameCase = (a,b) => /[a-z]/i.test(a) && /[a-z]/i.test(b) ? Number(/[a-z]/.test(a) == /[a-z]/.test(b)) : -1;
+
+// function sameCase(a, b){
+//     if(a.toLowerCase() === a.toUpperCase() || b.toUpperCase() === b.toLowerCase()){
+//         return -1
+//     } else if (a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+//         return 1
+//     } else {
+//         return 0
+//     }
+// }
+
+
+//Remove First and Last Character
+// const removeChar = str => {
+//     return str.slice(1, str.length - 1)
+// }
+
+
+
+// //Sum of positive
+// function positiveSum(arr) {
+//     let sumNum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//       if (arr[i] > 0){
+//       sumNum += arr[i];
+//     //   console.log(sumNum);
+//       } else (arr[i] <= 0)
+//         continue;
+//       }
+//       return sumNum;
+//     }
+//   positiveSum([1,2,3])
+
+
+
+// //Remove String Spaces
+// function noSpace(x) {
+//     let newStr = ''
+//     for(let i = 0; i < x.length; i++) {
+//         if(x[i] !== " "){
+//             newStr += x[i]
+//         }
+//     }
+//     return newStr
+// }
+
+// //Beginner lost whith out map
+// function maps(x){
+//     return x.map(n => n * 2);
+//     }
