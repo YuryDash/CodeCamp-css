9768073c8ad2778dc92646ce6d285178ec3451f9
Yury <ydaszukiewicz@gmail.com>
2022-10-13 21:12:44 +0300

    13.10 endCodeCamp

--- start.js
+++ start.js
@@ -287,6 +287,31 @@
 // [a, b] = [b, a];
 
 
+// //Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+
+// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function removeFirstTwo(list) {
+  
+//   const [a, b, ...arr] = list;
+//   return arr;
+// }
+// const arr = removeFirstTwo(source);
+// console.log(arr);
+// console.log(source);
+
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+//   };
+  
+//   const half = ({ max, min }) => (max + min) / 2.0;
+  
 
 
 
