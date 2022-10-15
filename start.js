3a698f5eef91d732d7c67fb21a9d3cc9c120140d
Yury <ydaszukiewicz@gmail.com>
2022-10-15 19:41:18 +0300

    15.10

--- start.js
+++ start.js
@@ -510,17 +510,17 @@
 // userInfo(userProfile);
 
 
-//if в функциях 
-const sumPositiveNumbers = (a , b) => {
-    if(typeof a !== 'Number' || typeof b !== 'Number') {
-        return 'One of the arguments is not a number'
-    }
-    if(a <= 0 || b <= 0 ) {
-        return ' numbers are not positive'
-    }
-    return a + b 
-}
-console.log(sumPositiveNumbers(10, 10))
+// //if в функциях 
+// const sumPositiveNumbers = (a , b) => {
+//     if(typeof a !== 'number' || typeof b !== 'number') {
+//         return 'One of the arguments is not a number'
+//     }
+//     if(a <= 0 || b <= 0 ) {
+//         return ' numbers are not positive'
+//     }
+//     return a + b 
+// }
+// console.log(sumPositiveNumbers(10, 10));
 
 
 
