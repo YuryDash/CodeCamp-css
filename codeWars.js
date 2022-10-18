6c18666721722714fdbe91155ee932c44251bd29
Yury <ydaszukiewicz@gmail.com>
2022-10-18 23:18:33 +0300

    18.10 codeWars

--- codeWars.js
+++ codeWars.js
@@ -21,16 +21,85 @@
 //     }
 
 
-//
-function lovefunc(flower1, flower2){
-    if(flower1 % 2 == 0 && flower2 % 2 == 1){
-       return true;
-   }
-    if(flower1 % 2 == 1 && flower2 % 2 == 0){
-      return true;
-   }
-   else{
-       return false;
-       }
-   }
+// //
+// function lovefunc(flower1, flower2){
+//     if(flower1 % 2 == 0 && flower2 % 2 == 1){
+//        return true;
+//    }
+//     if(flower1 % 2 == 1 && flower2 % 2 == 0){
+//       return true;
+//    }
+//    else{
+//        return false;
+//        }
+//    }
 
+
+// //Beginner Series #1 School Paperwork
+// function paperwork(n, m) {
+//     if (n < 0 || m < 0) {
+//       return 0;
+//     } else {
+//     return n * m;
+//       }
+//     }
+
+
+// // /How good are you really?
+// betterThanAverage = (classPoints, yourPoints) => 
+// classPoints.reduce((prev,curr) => prev + curr) / classPoints.length < yourPoints;
+
+// //Find the next perfect square!
+// function findNextSquare(sq) {
+//     var root = Math.sqrt(sq);
+//     if (Number.isInteger(root)) {
+//       return Math.pow(root + 1, 2);
+//     } else {
+//       return -1;
+//     }
+//   }
+
+
+// //Transportation on vacation
+// function rentalCarCost(d) {
+//     let s = 40;
+//     let w = d * s;
+//       if (d >= 3 && d < 7) return w - 20;
+//       if (d >= 7) return w - 50;
+//       if (d === 2) return w
+//       if (d === 1) return w
+//     }
+//     console.log(rentalCarCost(8))
+
+
+// //Calculate BMI
+// function bmi(weight, height) {
+//     let result = weight / (height * height)
+//     if (result <= 18.5) return 'Underweigth'
+//     if (result <= 25.0) return 'Normal'
+//     if (result <= 30.0) return 'Overweight'
+//     if (result > 30) return 'Obese'
+//   }
+
+
+// //You only need one - Beginner
+// function check(a, x) {
+//     for (let i = 0; i < a.length; i++) {
+//       if (a[i] == x) return true
+//     }
+//       return false
+//     }
+
+
+// //Total amount of points
+// function points(games) {
+//     let sum=0;
+//     for (let i=0; i<games.length; ++i)
+//     {
+//       if (games[i][0]>games[i][2])
+//         sum+=3;
+//       if (games[i][0]==games[i][2])
+//         sum+=1;
+//     }
+//     return sum;
+//   }
