1764ddad683937e18a06ba7d438d6cdc84eae764
Yury <ydaszukiewicz@gmail.com>
2022-10-21 23:39:06 +0300

    21.10/codeCamp

--- codeCamp__video.js
+++ codeCamp__video.js
@@ -214,3 +214,74 @@
 //     }
 //     return result.toFixed(2);
 //   };
+
+
+// //Extract Matches
+
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/;
+// let res = extractStr.match(codingRegex);
+
+
+// //Find More Than the First Match
+
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /twinkle/gi;
+// let result = twinkleStar.match(starRegex);
+
+
+// //Match Anything with Wildcard Period
+
+// let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un/;
+// let result = unRegex.test(exampleStr);
+
+
+// //Match Single Character with Multiple Possibilities
+
+// let quoteSample =
+//   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/gi;
+// let result = quoteSample.match(vowelRegex);
+
+
+// //Match Letters of the Alphabet
+
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi;
+// let result = quoteSample.match(alphabetRegex);
+
+
+// //Match Numbers and Letters of the Alphabet
+
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/gi;
+// let result = quoteSample.match(myRegex);
+
+
+// //Match Single Characters Not Specified
+
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^aeiuo0-9]/gi;
+// let result = quoteSample.match(myRegex);
+
+
+// //Match Characters that Occur One or More Times
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/gi;
+// let result = difficultSpelling.match(myRegex);
+
+
+// //Match Characters that Occur Zero or More Times
+
+// let chewieQuote = 'Aaaaaaaaaaaaaaaarrrgh!';
+// let chewieRegex = /Aa*/;
+// let result = chewieQuote.match(chewieRegex);
+
+
+// //Find Characters with Lazy Matching
+
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*?>/;
+// let result = text.match(myRegex);
